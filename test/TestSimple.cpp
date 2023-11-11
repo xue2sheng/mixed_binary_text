d60df5ef0e067a414317964a0777e023a946ac62
xue2sheng <andres.sevillano.vallejo@gmail.com>
2023-11-11 21:56:12 +0100

    adding unit tests

---
+++ test/TestSimple.cpp
@@ -0,0 +1,11 @@
+//
+// Created by user on 11/11/23.
+//
+
+#include "lib.h"
+#include <cstring>
+
+int main() {
+    return process("input.bin", strlen("input.bin"), "output.txt", strlen("output.txt"),
+        "", 0, "", 0, "", 0);
+}
